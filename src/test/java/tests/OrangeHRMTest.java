030a8a8f4d737fce4b90cdc632d357894fa55679
balaji ramasamy <baramasa27@gmail.com>
2022-04-05 10:40:01 +0530

    parameter added

--- src/test/java/tests/OrangeHRMTest.java
+++ src/test/java/tests/OrangeHRMTest.java
@@ -2,22 +2,34 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OrangeHRMTest {
+	WebDriver driver=null;
 	
-	
+	@Parameters("Browser")
 	@Test
-	public void titleTest() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	public void titleTest(String browserName) {
+		if(browserName.contains("chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browserName.contains("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "title does not match");
 		driver.quit();
-	}
+	} 
 	
 
 }
